f43ad5086faebd52c5547770dd485a93cf508e66
chlamydamanada <evil.cabaret@gmail.com>
2022-12-20 11:17:12 +0100

    try to catch error in comments_router.ts

--- src/routers/comments_router.ts
+++ src/routers/comments_router.ts
@@ -1,4 +1,4 @@
-import { Response, Router } from "express";
+import { Request, Response, Router } from "express";
 import { RequestWithURL, RequestWithUrlAndBody } from "../models/request_types";
 import { commentsQweryRepository } from "../repositories/comments_qwery_repository";
 import { commentsService } from "../domain/comments_service";
@@ -9,24 +9,21 @@ import { inputValMiddleware } from "../middlewares/inputValue.middleware";
 
 export const commentsRouter = Router();
 
-commentsRouter.get(
-  "/:commentId",
-  async (req: RequestWithURL<{ commentId: string }>, res: Response) => {
-    const comment = await commentsQweryRepository.findCommentById(
-      req.params.commentId
-    );
-    if (!comment) {
-      res.sendStatus(404);
-    } else {
-      res.status(200).send(comment);
-    }
+commentsRouter.get("/:commentId", async (req: Request, res: Response) => {
+  const comment = await commentsQweryRepository.findCommentById(
+    req.params.commentId
+  );
+  if (!comment) {
+    res.sendStatus(404);
+  } else {
+    res.status(200).send(comment);
   }
-);
+});
 commentsRouter.delete(
   "/:commentId",
   bearerAuthMiddleware,
   userIsOwnerOfCommentMiddleware,
-  async (req: RequestWithURL<{ commentId: string }>, res: Response) => {
+  async (req: Request, res: Response) => {
     const isDel = await commentsService.deleteComment(req.params.commentId);
     res.sendStatus(204);
   }
@@ -37,10 +34,7 @@ commentsRouter.put(
   userIsOwnerOfCommentMiddleware,
   contentOfCommentsMiddleware,
   inputValMiddleware,
-  async (
-    req: RequestWithUrlAndBody<{ commentId: string }, { content: string }>,
-    res: Response
-  ) => {
+  async (req: Request, res: Response) => {
     const newComment = await commentsService.updateComment(
       req.params.commentId,
       req.body.content
